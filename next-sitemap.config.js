beb8f90d248dea3c0b25d9ec798cd7a11991e8c5
vikaskhandekar001 <32407723+vikaskhandekar001@users.noreply.github.com>
2025-07-27 20:20:41 +0530

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.getcolorcodes.com',
-    generateRobotsTxt: true,
-  }
+  siteUrl: 'https://getcolorcodes.com',
+  generateRobotsTxt: true, // (optional)
+};
